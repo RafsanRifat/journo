c2f431e1a22bcd03d415b867255ebba62b9d4b10
RafsanRifat <rafsan.rifat255@gmail.com>
2023-02-12 12:35:08 +0600

    page rendering

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'blog/home.html')
